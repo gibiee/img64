e1c12abdb7b84d9bc8b237c2c7c06beabffd7cc4
gibiee <gibiee@naver.com>
2024-04-12 21:50:22 +0900

    function edit

--- img64/convert.py
+++ img64/convert.py
@@ -1,25 +1,34 @@
 import io
-import base64 as _base64
+import base64
 import numpy as np
 from PIL import Image
 from typing import Union, Literal
 
-def image_to_base64(image: Union[Image.Image, np.ndarray]) -> str :
-    if isinstance(image, np.ndarray) :
-        image = Image.fromarray(image)
+# img = Image.open('151.png')
 
+def image_to_bytes(img: Union[Image.Image, np.ndarray]) -> bytes :
+    if isinstance(img, np.ndarray) :
+        img = Image.fromarray(img)
+    
     buffer = io.BytesIO()
-    image.save(buffer, format="PNG")
+    img.save(buffer, format="PNG")
+    img_bytes = buffer.getvalue()
+    return img_bytes
 
-    image_bytes = buffer.getvalue()
-    image_b64 = _base64.b64encode(image_bytes)
-    image_b64_utf8 = image_b64.decode("utf-8")
+def image_to_base64(img: Union[Image.Image, np.ndarray]) -> str :
+    img_bytes = image_to_bytes(img)
+    img_b64 = base64.b64encode(img_bytes)
+    img_b64_utf8 = img_b64.decode("utf-8")
+    return img_b64_utf8
 
-    return image_b64_utf8
-
-def base64_to_image(base64: str, type: Literal['pil', 'numpy']='pil') -> Union[Image.Image, np.ndarray] :
+def base64_to_bytes(b64: str) -> bytes :
+    img_bytes = base64.b64decode(b64)
+    return img_bytes
+    
+def base64_to_image(b64: str, type: Literal['pil', 'numpy']='pil') -> Union[Image.Image, np.ndarray] :
     assert type in ['pil', 'numpy'], "Expected type 'pil' or 'numpy'"
-    img_bytes = _base64.b64decode(base64)
+    
+    img_bytes = base64_to_bytes(b64)
     buffer = io.BytesIO(img_bytes)
     img = Image.open(buffer)
     img = np.array(img) # RGB 이미지가 PIL.PngImagePlugin.PngImageFile 객체인 현상 방지
